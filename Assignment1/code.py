15b61fc7d8eb7c4d16810652f010738d3f71a361
piyuagr <57537384+piyuagr@users.noreply.github.com>
2021-05-27 09:35:37 +0530

    Update code.py

--- Assignment1/code.py
+++ Assignment1/code.py
@@ -1,4 +1,7 @@
 import hashlib
+import time
+
+start = time.time()
 
 s = input("Enter the string: ")
 i = 1
@@ -11,3 +14,6 @@ while (s1.hexdigest()) >=  "00000fffffffffffffffffffffffffffffffffffffffffffffff
     s1=hashlib.sha256(s1.encode())
     
 print(i)
+
+end = time.time()
+print(f"Runtime of the program is {end-start}")
